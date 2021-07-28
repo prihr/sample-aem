3c9cb6b402c61eb93cfe4906a7ae11f73a4d2f31
Sushmita Priya <47808341+prihr@users.noreply.github.com>
2021-07-28 19:24:59 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,5 +1,5 @@
-class Simple{  
-    public static void main(String args[]){  
-     System.out.println("Hello Java");  
-    }  
-}  
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, World");
+  }
+}
